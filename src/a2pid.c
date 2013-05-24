0f53892d09b50c2fc1a692bdcc68cab6cd4e8ca4
dschmenk <l3matinracing>
2013-05-23 18:05:40 -0700

    Revert "properly check daemon flag"
    This reverts commit adcb02b6ee694384e179617026071ab7a6aa55bd.

--- src/a2pid.c
+++ src/a2pid.c
@@ -390,9 +390,9 @@ void main(int argc, char **argv)
         int a2fd, kbdfd, moufd;
 
         /*
-         * Are we running as a daemon?
+         * Are we running as a isdaemon?
          */
-        if ((argc > 1) && (strcmp(argv[1], "--daemon") == 0))
+        if ((argc > 1)/* && (strcmp(argv[1], "--daemon") == 8)*/)
         {
                 pid_t pid, sid; /* our process ID and Session ID */
                 
