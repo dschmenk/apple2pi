6c813637fda7181fb82cd713d49a16b7ba3df5ef
dschmenk <l3matinracing>
2013-11-17 19:08:22 -0800

    slightly update ][ keymap, fix for text2merlin running off in the weeds

--- share/text2merlin.c
+++ share/text2merlin.c
@@ -3,22 +3,23 @@
 
 int main(int argc, char **argv)
 {
-	int fout;
-	char c;
-
-	if (argc > 1)
-		fout = open(argv[1], O_WRONLY|O_CREAT, 0644);
-	else
-		fout = 1;
-	while ((c = getc(stdin)) != EOF)
-	{
-		if (c == '\n')
-			c = '\r';
-		if (c == '\t')
-			c = ' ';
-		c |= 0x80;
-		write(fout, &c, 1);
-	}
-	close(fout);
-	return (0);
+    int cin, fout;
+    char c;
+    
+    if (argc > 1)
+	fout = open(argv[1], O_WRONLY|O_CREAT, 0644);
+    else
+	fout = 1;
+    while ((cin = getchar()) != EOF)
+    {
+	c = cin;
+	if (c == '\n')
+	    c = '\r';
+	if (c == '\t')
+	    c = ' ';
+	c |= 0x80;
+	write(fout, &c, 1);
+    }
+    close(fout);
+    return (0);
 }
